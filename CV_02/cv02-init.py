0234e4f94a74f7c5f73dd16f10d072e1758c5d24
Jakub Kindermann <49842007+kindejak@users.noreply.github.com>
2023-03-11 21:02:36 +0100

    Revert "first commit"
    This reverts commit 70a3e3295a09bbc288a5950ef234173c855820cc.

--- CV_02/cv02-init.py
+++ CV_02/cv02-init.py
@@ -1,63 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import math
 import os
-def to_hue(img):
-    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-    return hsv[:,:,0]
-
-def mean_shift(img, x1, y1, x2, y2, hist):
-    # transform to hue
-    hue = to_hue(img)
-    # while not converged
-    while True:
-        # Select pixels
-        mask = np.zeros_like(hue)
-        mask[y1:y2, x1:x2] = 1
-        # Compute histogram
-        hist, b = np.histogram(hue[mask==1], 256, (0, 256))
-        # Compute mean
-        mean = np.sum(hist * np.arange(256)) / np.sum(hist)
-        # Compute variance
-        var = np.sum(hist * (np.arange(256) - mean)**2) / np.sum(hist)
-        # Compute new window
-        x1 = int(mean - 2 * np.sqrt(var))
-        x2 = int(mean + 2 * np.sqrt(var))
-        y1 = int(mean - 2 * np.sqrt(var))
-        y2 = int(mean + 2 * np.sqrt(var))
-        # Check convergence
-        if np.abs(mean - var) < 1:
-            break
-    return x1, y1, x2, y2
+plt.ion()
+def clear(): return os.system('cls')
 
 
+clear()
+plt.close('all')
 
 
-# main if
-if __name__ == '__main__':
-    plt.ion()
-    clear = lambda: os.system('cls')
-    clear()
-    plt.close('all')
+# Read in the image
+image = cv2.imread('cv02_vzor_hrnecek.bmp')
 
-    cap = cv2.VideoCapture('cv02_hrnecek.mp4')
+# convert to HSV
+HSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    x1 = 100
-    y1 = 100
-    x2 = 200
-    y2 = 200
+# extract the Hue channel
+Hue = HSV[:,:,0]
 
+# search for the 
 
-    while True:
-        ret, bgr = cap.read()
-        if not ret:
-            break
+# histogram of Hue
+# hist, b = np.histogram(hsv[:,:,0], 256, (0, 256))
+hist = cv2.calcHist([Hue], [0], None, [180], [0, 180])
+# normalize the histogram
+normalized_hist = cv2.normalize(hist, hist, 0, 1, cv2.NORM_MINMAX)
+
+# plot the histogram
+plt.plot(normalized_hist)
+
+cap = cv2.VideoCapture('cv02_hrnecek.mp4')
+
+while True:
+    ret, bgr = cap.read()
+    if not ret:
+        break
+    hsv = cv2.cvtColor(bgr, cv2.COLOR_RGB2HSV)    
+    #calcBackProject
+    P = cv2.calcBackProject([hsv], [0], normalized_hist, [0, 180], 1)
+    
+    #calc center of gravity
+    x_p, y_p = 0, 0
+    for i in range(P.shape[0]):
+        for j in range(P.shape[1]):
+            x_p += i * P[i,j]
+            y_p += j * P[i,j]
+            
+    x_t = x_p / P.sum()
+    y_t = y_p / P.sum()
         
-
-        cv2.rectangle(bgr, (x1, y1), (x2, y2), (0, 255, 0))
-        cv2.imshow('Image', bgr)
-        key = 0xFF & cv2.waitKey(30)
-        if key == 27:
-            break
         
-    cv2.destroyAllWindows()
+    # draw rectangle
+    s = 0.7 * math.sqrt(P.sum())
+    v = 0.9 * math.sqrt(P.sum())
+    
+    cv2.rectangle(bgr, (int(y_t-s/2), int(x_t-s/2)), (int(y_t+v/2), int(x_t+v/2)), (0, 255, 0))
+    cv2.imshow('Image', bgr)
+    
+    key = 0xFF & cv2.waitKey(30)
+    if key == 27: 
+        break
+
+cv2.destroyAllWindows()
